e8e2d9220dc88a8f433dc751c2a45a0973bc6419
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2022-12-14 19:17:45 +0600

    Initial commit

---
+++ app/src/main/java/com/yrys/kotlin5_group46_lesson8_git/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yrys.kotlin5_group46_lesson8_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
